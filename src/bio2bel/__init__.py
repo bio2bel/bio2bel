5a449d14d0bfc7316268149b58ddf8308e225ea0
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-11 18:30:58 +0200

    Add extension loader based
    This is based on @scolby33’s extension loader for PyBEL, but even more
    ridiculous since it’s sticking stuff on the top level without any
    remorse

---
+++ src/bio2bel/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from pkg_resources import iter_entry_points
+
+GROUP_NAME = 'bio2bel'
+
+# Allow `from bio2bel.ext import *`
+entries = [
+    entry_point.name
+    for entry_point in iter_entry_points(group=GROUP_NAME, name=None)
+]
+
+
+def setup():
+    """Add the :code:`bio2bel.ext` importer/loader to the meta_path. Should probably only be called once."""
+    from .exthook import ExtensionImporter
+    importer = ExtensionImporter(GROUP_NAME)
+    importer.install()
+
+
+setup()
+del setup  # Do a bit of cleanup. Not sure if it's necessary, but Flask did...
+
+__version__ = '0.0.1-dev'
+
+__title__ = 'bio2bel'
+__description__ = "A package for converting biological stuff to BEL"
+__url__ = 'https://github.com/bio2bel/bio2bel'
+
+__author__ = 'Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017 Charles Tapley Hoyt'
+
+__all__ = entries
