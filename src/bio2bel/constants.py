890530b42e85a2715f41c1b5375701a24a19b014
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-12 10:52:14 +0100

    Added constants

---
+++ src/bio2bel/constants.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+BIO2BEL_DIR = os.environ.get('BIO2BEL_DIRECTORY', os.path.join(os.path.expanduser('~'), '.pybel', 'bio2bel'))
+os.makedirs(BIO2BEL_DIR, exist_ok=True)
+
+DEFAULT_CACHE_NAME = 'bio2bel.db'
+DEFAULT_CACHE_PATH = os.path.join(BIO2BEL_DIR, DEFAULT_CACHE_NAME)
+DEFAULT_CACHE_CONNECTION = os.environ.get('BIO2BEL_CONNECTION', 'sqlite:///' + DEFAULT_CACHE_PATH)
