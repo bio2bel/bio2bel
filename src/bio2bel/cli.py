daf10b0c004e3fc2c2ae861b3307cb72beb95500
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-12 10:51:52 +0100

    Update CLI
    - Better logging
    - Added loader for web and populate functions

--- src/bio2bel/cli.py
+++ src/bio2bel/cli.py
@@ -13,25 +13,43 @@ log = logging.getLogger(__name__)
 
 modules = {}
 commands = {}
+deploy_commands = {}
+populate_commands = {}
+web_commands = {}
 
 for entry in entries:
-    try:
+    try:  # TODO can be replaced by entry_point.resolve()
         bio2bel_module = importlib.import_module('bio2bel_{}'.format(entry))
-    except:
-        log.warning("can't import module: %s", entry)
+    except ImportError:
+        log.exception("can't import bio2bel_%s", entry)
         continue
 
     try:
-        modules[entry] = bio2bel_module.cli
-    except:
-        log.warning('%s has no CLI', entry)
+        modules[entry] = importlib.import_module('bio2bel_{}.cli'.format(entry))
+    except ImportError:
+        log.warning('no submodule bio2bel_%s.cli', entry)
         continue
 
     try:
         commands[entry] = modules[entry].main
-    except:
-        log.warning('%s has no command: main', entry)
+    except NameError:
+        log.warning('no command group bio2bel_%s.cli:main', entry)
         continue
+
+    try:
+        deploy_commands[entry] = modules[entry].deploy
+    except AttributeError:
+        log.debug('no command bio2bel_%s.cli:deploy', entry)
+
+    try:
+        populate_commands[entry] = modules[entry].populate
+    except AttributeError:
+        log.debug('no command bio2bel_%s.cli:populate', entry)
+
+    try:
+        web_commands[entry] = modules[entry].web
+    except AttributeError:
+        log.debug('no command bio2bel_%s.cli:web', entry)
 
 main = click.Group(commands=commands)
 
@@ -43,14 +61,20 @@ def util():
 
 @util.command()
 @click.pass_context
+def populate(ctx):
+    """Runs all populate commands"""
+    for name, command in populate_commands.items():
+        click.echo('populating {}'.format(name))
+        command.invoke(ctx)
+
+
+@util.command()
+@click.pass_context
 def deploy(ctx):
     """Runs all deploy commands"""
-    for name, mod in modules.items():
-        try:
-            click.echo('Deploying {}'.format(name))
-            mod.deploy.invoke(ctx)
-        except:
-            click.echo('No deploy function found for {}'.format(name))
+    for name, command in deploy_commands.items():
+        click.echo('deploying {}'.format(name))
+        command.invoke(ctx)
 
 
 if __name__ == '__main__':
