fb46c838b97d93d944587b00050ba848ef7bf019
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-25 18:11:33 +0100

    Bump version: 0.0.1-dev → 0.0.1

--- src/bio2bel/__init__.py
+++ src/bio2bel/__init__.py
@@ -21,7 +21,7 @@ def setup():
 setup()
 del setup  # Do a bit of cleanup. Not sure if it's necessary, but Flask did...
 
-__version__ = '0.0.1-dev'
+__version__ = '0.0.1'
 
 __title__ = 'bio2bel'
 __description__ = "A package for converting biological stuff to BEL"
