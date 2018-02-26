50c77d9c4f2c28d537be61769d255ab5fd7c4047
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-26 10:49:49 +0100

    Add class methods to make easy user interface

--- src/bio2bel/models.py
+++ src/bio2bel/models.py
@@ -3,9 +3,15 @@
 """Bio2BEL database model"""
 
 import datetime
+import logging
 
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, DateTime, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from bio2bel.constants import get_global_connection
+
+log = logging.getLogger(__name__)
 
 Base = declarative_base()
 
@@ -23,3 +29,54 @@ class Action(Base):
                       doc='The normalized name of the Bio2BEL package (e.g., hgnc, chebi, etc)')
     action = Column(String(32), nullable=False)
     created = Column(DateTime, nullable=False, default=datetime.datetime.utcnow, doc='The date and time of upload')
+
+    @classmethod
+    def make_populate(cls, resource):
+        """Make a ``populate`` instance of :class:`Action`"""
+        return Action(resource=resource.lower(), action='populate')
+
+    @classmethod
+    def make_drop(cls, resource):
+        """Make a ``drop`` instance of :class:`Action`"""
+        return Action(resource=resource.lower(), action='drop')
+
+    @classmethod
+    def _store_helper(cls, make_method, resource):
+        connection = get_global_connection()
+
+        engine = create_engine(connection)
+        Base.metadata.create_all(engine, checkfirst=True)
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        model = make_method(resource)
+        session.add(model)
+        session.commit()
+        session.close()
+
+    @classmethod
+    def store_populate(cls, resource):
+        """Stores a drop event
+
+        :param str resource: The normalized name of the resource to store
+
+        >>> from bio2bel.models import Action
+        >>> Action.store_populate('hgnc')
+        """
+        cls._store_helper(cls.make_populate, resource)
+
+    @classmethod
+    def store_drop(cls, resource):
+        """Stores a drop event
+
+        :param str resource: The normalized name of the resource to store
+
+        >>> from bio2bel.models import Action
+        >>> Action.store_drop('hgnc')
+        """
+        cls._store_helper(cls.make_drop, resource)
+
+
+if __name__ == '__main__':
+    Action.store_populate('hgnc')
