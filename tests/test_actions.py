2c8ea6de6a289f38a0d3afa3939dfff73340e3bf
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-26 11:37:31 +0100

    Add test with mocks

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -4,6 +4,7 @@ import logging
 import os
 import tempfile
 import unittest
+from unittest import mock
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -48,6 +49,42 @@ class TestActions(unittest.TestCase):
 
         self.assertIsNotNone(action)
         self.assertEqual('test', action.resource)
+
+
+class TestActionMock(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Create temporary file"""
+        cls.fd, cls.path = tempfile.mkstemp()
+        cls.connection = 'sqlite:///' + cls.path
+
+        def mock_get_global_connection():
+            return cls.connection
+
+        cls.patch = mock.patch('bio2bel.models.get_global_connection', mock_get_global_connection)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Closes the connection in the manager and deletes the temporary database"""
+        os.close(cls.fd)
+        os.remove(cls.path)
+
+    def test_decorator(self):
+        with self.patch:
+            self.assertEqual(0, len(Action.ls()), msg='should not already have entries')
+
+            @bio2bel_populater('test')
+            def populate_something():
+                """A dummy function that represents a population function. May even be a class function."""
+
+            populate_something()
+
+            actions = Action.ls()
+            self.assertEqual(1, len(actions), msg='entry was not stored')
+            action = actions[0]
+
+            self.assertIsNotNone(action)
+            self.assertEqual('test', action.resource)
 
 
 if __name__ == '__main__':
