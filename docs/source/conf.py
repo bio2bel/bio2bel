2b0e9b088aee0ded033ffd58091ee3b17b1787d6
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-10 17:33:47 +0200

    Bump version: 0.0.11 → 0.0.12-dev
    [skip ci]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,7 @@ author = 'Charles Tapley Hoyt'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '0.0.11'
+release = '0.0.12-dev'
 
 # The short X.Y version.
 parsed_version = re.match(
