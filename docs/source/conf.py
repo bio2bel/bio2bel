e3a3ba09f4145d9d616099d677ce74c8e5923abf
Charles Tapley Hoyt <cthoyt@wlan-200.scai.fraunhofer.de>
2018-06-19 15:51:37 +0200

    Bump version: 0.0.13 → 0.0.14-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,7 @@ author = 'Charles Tapley Hoyt'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '0.0.13'
+release = '0.0.14-dev'
 
 # The short X.Y version.
 parsed_version = re.match(
