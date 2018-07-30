9897c6819b51678d9f151de261116c7a7cf67962
Charles Tapley Hoyt <cthoyt@wlan-224.scai.fraunhofer.de>
2018-07-30 11:03:46 +0200

    Bump version: 0.1.1-dev → 0.1.1

--- src/bio2bel/constants.py
+++ src/bio2bel/constants.py
@@ -8,7 +8,7 @@ import os
 
 log = logging.getLogger(__name__)
 
-VERSION = '0.1.1-dev'
+VERSION = '0.1.1'
 
 BIO2BEL_DIR = os.environ.get('BIO2BEL_DIRECTORY', os.path.join(os.path.expanduser('~'), '.pybel', 'bio2bel'))
 os.makedirs(BIO2BEL_DIR, exist_ok=True)
