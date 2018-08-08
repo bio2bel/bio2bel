3c0ebd116219a8294efd35b05258a6c22d82f8f1
Charles Hoyt <cthoyt@microvac.speedport.ip>
2018-08-08 23:57:25 +0200

    Bump version: 0.1.3-dev → 0.1.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,7 @@ author = 'Charles Tapley Hoyt'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '0.1.3-dev'
+release = '0.1.3'
 
 # The short X.Y version.
 parsed_version = re.match(
