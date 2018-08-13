4721f3727cdad366c15a235e7600804e3e09ffab
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-08-13 10:32:54 +0200

    Bump version: 0.1.4 → 0.1.5-dev
    [skip ci]

--- src/bio2bel/constants.py
+++ src/bio2bel/constants.py
@@ -8,7 +8,7 @@ from configparser import ConfigParser
 
 log = logging.getLogger(__name__)
 
-VERSION = '0.1.4'
+VERSION = '0.1.5-dev'
 
 BIO2BEL_DIR = os.environ.get('BIO2BEL_DIRECTORY', os.path.join(os.path.expanduser('~'), '.pybel', 'bio2bel'))
 os.makedirs(BIO2BEL_DIR, exist_ok=True)
