c342f39df929144a6180decc435457f71568a5c4
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-12-20 16:01:59 -0500

    Bump version: 0.2.1 → 0.2.2-dev
    [skip ci]

--- src/bio2bel/constants.py
+++ src/bio2bel/constants.py
@@ -9,7 +9,7 @@ from easy_config import EasyConfig
 
 log = logging.getLogger(__name__)
 
-VERSION = '0.2.1'
+VERSION = '0.2.2-dev'
 
 DEFAULT_CONFIG_DIRECTORY = os.path.abspath(os.path.join(os.path.expanduser('~'), '.config', 'bio2bel'))
 DEFAULT_CONFIG_PATHS = [
