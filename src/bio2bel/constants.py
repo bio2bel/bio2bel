347888c46329142e65a0f5eab5b35517ea785cc7
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-03-05 07:52:09 +0100

    Update constants.py

--- src/bio2bel/constants.py
+++ src/bio2bel/constants.py
@@ -34,12 +34,8 @@ class Config(EasyConfig):
     #: The SQLAlchemy connection string to the database
     connection: str = f'sqlite:///{os.path.join(directory, default_cache_name)}'
 
-    @classmethod
-    def load(cls, *args, **kwargs):
-        """Load the Bio2BEL configuration and ensure the directory."""
-        rv = super().load(*args, **kwargs)
-        os.makedirs(rv.directory, exist_ok=True)
-        return rv
+    def __post_init__(self):
+        os.makedirs(self.directory, exist_ok=True)
 
 
 config = Config.load(_lookup_config_envvar='config')
