f6e9c47938c1f2af2376be7f14ff93f83ef97673
Charles Tapley Hoyt <cthoyt@galvac.speedport.ip>
2020-03-16 17:09:26 +0100

    Bump version: 0.3.4-dev → 0.3.4

--- src/bio2bel/constants.py
+++ src/bio2bel/constants.py
@@ -10,7 +10,7 @@ from easy_config import EasyConfig
 
 log = logging.getLogger(__name__)
 
-VERSION = '0.3.4-dev'
+VERSION = '0.3.4'
 
 _USER_CONFIG_DIRECTORY = os.path.abspath(os.path.join(os.path.expanduser('~'), '.config'))
 DEFAULT_CONFIG_DIRECTORY = os.path.join(_USER_CONFIG_DIRECTORY, 'bio2bel')
